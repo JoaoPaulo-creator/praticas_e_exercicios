c28d62d297f1a390c347fb5f9333fd219ac3ea4f
João Paulo <79981086+JoaoPaulo-creator@users.noreply.github.com>
2021-03-10 16:09:53 -0300

    Update consultaCEP.py

--- consultaCEP.py
+++ consultaCEP.py
@@ -9,9 +9,10 @@ driver.get('https://buscacepinter.correios.com.br/app/endereco/index.php?t')
 
 
 #Digitando o endereço no campo "Digite um CEP ou um Endereço:"
-lograouro = driver.find_element_by_id('endereco')
-lograouro.send_keys('nome da rua')
-#lograouro.send_keys('a')
+logradouro = driver.find_element_by_id('endereco')
+#logradouro.send_keys('Rua Antonio Rogerio da Silva Rosa')
+a = 'a'
+logradouro.send_keys(a)
 
 
 # As linhas abaixo servem para selecionar uma opção do campo "Esse CEP é de:"
@@ -26,16 +27,51 @@ select.select_by_visible_text('Localidade/Logradouro')
 time.sleep(1)
 driver.find_element_by_id('btn_pesquisar').click()
 
+try:
+    #Se o endereço informado não cumprir com os requisitos, o código vai cair neste bloco try
+    #Onde será realizado um novo processo de consulta 
 
-#Se o endereço digitado possuir menos que 2 caracteres, o script deve instruir um refresh na tela, caso o código dentro do bloco try for inválido, \n
-# deverá ser retornado ao tester uma mensagem e o navegador será fechado
+    time.sleep(1)
+    msg_alerta = driver.find_element_by_class_name('msg')
+    if msg_alerta.is_enabled() or msg_alerta.is_displayed():
+        print('O endereço informando possui menos de dois caracteres ou está incorreto')
+        time.sleep(1)
+        driver.refresh()
+
+        b = str(input('Informe um novo nome de rua: '))
+        #Se o logradouro informado possuir menos de dois caracteres, novamente será apresentado uma mensagem e novegador será fechado (não pensei em nada melhor a ser feito)
+        if len(b) < 2:
+            print('O logradouro  possui menos de 2 caracteres')
+            driver.quit()
+        else:
+            #Senão, dessa vez será possível efetuar a consulta
+            time.sleep(1)
+            logradouro = driver.find_element_by_id('endereco')
+            logradouro.send_keys(b)
+            time.sleep(1)
+            driver.find_element_by_id('btn_pesquisar').click()
+
+        
+
+except:
+    print('Não funcionou')
+    time.sleep(2)
+
+
+
+
+
+'''
+Isso tudo aqui em baixo é lixo
 
 try:
     time.sleep(1)
     msg_alerta = driver.find_element_by_class_name('msg')
     if msg_alerta.is_enabled() or msg_alerta.is_displayed():
         driver.refresh()
+
 except:
     print('Não funcionou')
     time.sleep(5)
     driver.quit()
+'''
